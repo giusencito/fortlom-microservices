30e62b3c2f54c1673ded4f44e37ecc54fa774009
giusencito <66499088+giusencito@users.noreply.github.com>
2022-05-17 17:33:22 -0500

    change controller

--- support-service/src/main/java/com/example/supportservice/api/SupportController.java
+++ support-service/src/main/java/com/example/supportservice/api/SupportController.java
@@ -20,7 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/supports")
+@RequestMapping("/support")
 public class SupportController {
 
     @Autowired
