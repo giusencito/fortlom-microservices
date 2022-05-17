ec2f4cd364aa26cd020ffacdf97174eab9ecee90
giusencito <66499088+giusencito@users.noreply.github.com>
2022-05-17 17:57:04 -0500

    Revert "change controller"
    This reverts commit 30e62b3c2f54c1673ded4f44e37ecc54fa774009.

--- support-service/src/main/java/com/example/supportservice/api/SupportController.java
+++ support-service/src/main/java/com/example/supportservice/api/SupportController.java
@@ -20,7 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/support")
+@RequestMapping("/api/v1/supports")
 public class SupportController {
 
     @Autowired
